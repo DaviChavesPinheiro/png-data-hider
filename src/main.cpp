4805428f08b8a110f359afb683ab3c180c1840c6
DaviChavesPinheiro <davichavespinheiro011001@gmail.com>
2022-01-10 20:24:36 -0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <fstream>
 #include <cerrno>
+#include <cstdint>
+#include <cassert>
 
 using namespace std;
 
@@ -10,29 +12,38 @@ int main(int argc, char const *argv[])
 {
     string program = *argv++;
     
-    if (*argv == NULL)
-    {
+    if (*argv == NULL) {
         cerr << "Usage:" << program << " <input.png>\n";
         exit(1);
     }
     
     string input_filepath = *argv++;
-    cout << "Inspected file is " << input_filepath << "\n";
+    cout << "Inspected file is " << input_filepath << ".\n";
 
     // Openning the file
-    streampos f_size;
-    char *memblock;
-
-    ifstream file (input_filepath, ios::in | ios::binary | ios::ate);
-    if (file.is_open())
-    {
-        f_size = file.tellg();
-        memblock = new char[f_size];
-        file.seekg(0, ios::beg);
-        file.close();
-    } else {
-        cerr << "Unable to open file " << input_filepath <<": " << strerror(errno) << "\n";
+    ifstream file (input_filepath, ios::in | ios::binary);
+    if (!file.is_open()) {
+        cerr << "[Error]: Unable to open file " << input_filepath <<": " << strerror(errno) << ".\n";
         exit(1);
+    }
+
+    uint8_t sig[8];
+    if(!file.read((char*) sig, sizeof(sig))) {
+        if(file.eof()) {
+            cout << "[Error]: The image is tool small.\n";
+        } else if(file.fail())
+        {
+            cout << "[Error]: Something went wrong when opening the image.\n";
+        } else
+        {
+            assert(0 && "unreachable");
+        }
+        exit(1);
+    }
+    
+    for (size_t i = 0; i < sizeof(sig); i++)
+    {
+        cout << (int)sig[i] << " ";
     }
     
     
