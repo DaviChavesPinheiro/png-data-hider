d8b23227467902591391b074e1ff5b89e2707a13
DaviChavesPinheiro <davichavespinheiro011001@gmail.com>
2022-01-10 16:34:13 -0300

    Openning file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <cerrno>
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello World\n";
+    string program = *argv++;
+    
+    if (*argv == NULL)
+    {
+        cerr << "Usage:" << program << " <input.png>\n";
+        exit(1);
+    }
+    
+    string input_filepath = *argv++;
+    cout << "Inspected file is " << input_filepath << "\n";
+
+    // Openning the file
+    streampos f_size;
+    char *memblock;
+
+    ifstream file (input_filepath, ios::in | ios::binary | ios::ate);
+    if (file.is_open())
+    {
+        f_size = file.tellg();
+        memblock = new char[f_size];
+        file.seekg(0, ios::beg);
+        file.close();
+    } else {
+        cerr << "Unable to open file " << input_filepath <<": " << strerror(errno) << "\n";
+        exit(1);
+    }
+    
+    
     return 0;
 }
